92f698770b6cb46a52c6c6c814e87e3b2bfeffb2
bavdinjuneja <bavdinjuneja.@gmail.com>
2025-08-28 17:52:53 +0530

    Update khushi.js

--- Priyansh/commands/khushi.js
+++ Priyansh/commands/khushi.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 module.exports.config = {
-  name: "khushi",
+  name: "jannat",
     version: "1.0.1",
   hasPermssion: 0,
   credits: "FAIZ ANSARI", 
@@ -12,13 +12,13 @@ module.exports.config = {
 
 module.exports.handleEvent = function({ api, event, client, __GLOBAL }) {
   var { threadID, messageID } = event;
-  if (event.body.indexOf("KHUSHI")==0 || event.body.indexOf("Khushi")==0 || event.body.indexOf("khushi")==0 || event.body.indexOf("@Khushi")==0) {
+  if (event.body.indexOf("JANNAT")==0 || event.body.indexOf("jannat")==0 || event.body.indexOf("jant")==0 || event.body.indexOf("Jannt")==0) {
     var msg = {
-        body: "❤️𝐘𝐄 𝐋𝐎 𝐊𝐇𝐔𝐒𝐇𝐈 𝐉𝐈 𝐀𝐀 𝐆𝐀𝐈🙈",
-        attachment: fs.createReadStream(__dirname + `/cache/khusi.jpeg`)
+        body: "❤️𝐘𝐄 𝐋𝐎 𝐉𝐀𝐍𝐍𝐀𝐓 𝐉𝐈 𝐀𝐀 𝐆𝐀𝐈🙈",
+        attachment: fs.createReadStream(__dirname + `/cache/khusi.`)
       }
       api.sendMessage(msg, threadID, messageID);
-    api.setMessageReaction("🙆‍♀️Khushi", event.messageID, (err) => {}, true)
+    api.setMessageReaction("🙆‍♀️JANNAT", event.messageID, (err) => {}, true)
     }
   }
   module.exports.run = function({ api, event, client, __GLOBAL }) {
